c57f1a9c2ae69ce172df30f21feec0c854dad727
dondi <dondi@lmu.edu>
2013-04-30 00:08:29 -0700

    Provided feedback for HW 0423 with corresponding update to standards development report.

--- homework/page-translation/addressTranslation.c
+++ homework/page-translation/addressTranslation.c
@@ -12,11 +12,20 @@ void setPageTable(pagetable *pt) {
     ptr = pt;
 }
 
-int getPhysical(int logical) {    
+int getPhysical(int logical) {
+    // JD: In terms of spacing, you should put a space after
+    //     the if and before the opening curly brace:
+    //
+    // if ((logical < 0) || (logical > 255)) {
     if((logical < 0) || (logical > 255)){
         return ERR_OUT_OF_RANGE;
     }
 
+    // JD: This is right, but better to use the PAGESIZE,
+    //     PAGEBITS, and PAGEMASK macros defined in the .h
+    //     file---if you do it that way, changing those
+    //     will automatically adapt your code to different
+    //     address register and page size settings.
     int offset = logical & 0xF;
     int page = (logical & 0xF0) >> 4;
     pagetable workingPT = ptr[page];
