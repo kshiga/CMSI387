740256dfb399ed5aecf7923c12c16e7a1a672d0b
Kaitlyn Higa <khiga1@lion.lmu.edu>
2013-07-09 13:37:09 -0700

    separated arguments and commands

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
  * This program demonstrates the use of the fork() and exec()
@@ -8,39 +9,76 @@
  */
 int main() {
     int running = 1;
+    int maxcomp = 256;
+    const int MAX_INPUT = 4096;
+    const int MAX_COMM = 256;
+    const int MAX_ARGS = 3840;
     const char *exit = "exit";
-    const char *cd = "exit";
+    const char *cd = "cd";
+    const char *and = "&";
+
+    char input[MAX_INPUT];
+    char command[MAX_COMM];
+    char arguments[MAX_ARGS];
+    char *parse;
+    int spaceloc = 0;
 
     while (running){
-        /* String to hold the command to run. */
-        char command[256];
-        printf("Enter the command to run: ");
-        scanf("%s", command);
+    
+        memset(input, '\0', MAX_INPUT);
+        memset(command, '\0', MAX_COMM);
+        memset(arguments, '\0', MAX_ARGS);
 
-        /* Variable that will store the fork result. */
+        
+        printf("Enter the command to run: ");
+        fgets(input, MAX_INPUT, stdin);
+        parse = strchr(input, ' ');
+        
+        
+        
+        if(!parse){            
+            strncpy(command, (const char *)input, ((int)strlen(input) - 1)); 
+        } else {
+            spaceloc = (int)(parse - input + 1);
+            strncpy(arguments, (const char *)parse, ((int)strlen(parse)-1));
+            strncpy(command, (const char *)input, (spaceloc -1));
+        }
+        
+
+
+
         pid_t pid;
 
         if (!strcmp(command, exit)) {
            running = 0;
+           printf("running is now %d\n", running);
         }
+        
+        if(!strncmp((const char *)command, cd, maxcomp)){
+            printf("command %s matched cd!\n", command);
+        } 
 
-        /* Perform the actual fork. */
+
+
         pid = fork();
         if (pid < 0) {
-            /* Error condition. */
             fprintf(stderr, "Fork failed\n");
             return -1;
         } else if (pid == 0) {
-            /* Child process. */
             printf("Running...\n");
-            execlp(command, command, NULL);
+            
+            if(strcmp(arguments, "")){
+                execlp(command, command, &arguments, NULL);
+            } else {
+                execlp(command, command, NULL);
+            }
+            
         } else {
-            /* Parent process. */
             int result;
             wait(&result);
-            printf("All done; result = %d\n", result);
+            //printf("All done; result = %d\n", result);
         }
-            printf("running value: %d\n", running);
-
+            //printf("running value: %d\n", running);
+            
     }
 }
