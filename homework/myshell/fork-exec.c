52d8ed05cf7e5a91b4bee0e68f6b924fcc981708
Kaitlyn Higa <khiga1@lion.lmu.edu>
2013-08-13 11:02:00 -0700

    fixed multiple arguments issue

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -17,51 +17,48 @@ int main() {
     const char *exit = "exit";
     const char *cd = "cd";
     const char *and = "&";
+    const char *argv[MAX_ARGS];
 
 
     char input[MAX_INPUT];
     char command[MAX_COMM];
     char *arg;
     char argumentsin[MAX_ARGS];
-    char arguments[MAX_ARGS];
     char *parse;
     int spaceloc = 0;
     int numspace = 0;
-    int i = 0;
+    int i = 1;
 
     while (!running){
-    
+        i = 1;    
         memset(input, '\0', MAX_INPUT);
         memset(command, '\0', MAX_COMM);
         memset(argumentsin, '\0', MAX_ARGS);
-
         
         printf("Enter the command to run: ");
         fgets(input, MAX_INPUT, stdin);
-        //printf("input: %s\n", input);
-        parse = strchr(input, ' ');
-        
-        
+        parse = strchr(input, ' ');      
         
         if(!parse){            
             strncpy(command, (const char *)input, ((int)strlen(input) - 1)); 
         } else {
+            argv[0] = command;
             spaceloc = (int)(parse - input + 1);
             strncpy(argumentsin, (const char *)parse, ((int)strlen(parse)-1));
             memmove (argumentsin, argumentsin+1, strlen (argumentsin));
             arg = strtok(argumentsin, " ");
             
-            for(i; i < numspace; i++){
-            
+            while(arg != NULL){
+                argv[i] = arg;
+                printf("argument: \"%s\"\n argv[%d]: \"%s\" \n", arg, i, argv[i]);
+                arg = strtok(NULL, " ");
+                i++;                
             }
-            
-            //printf("arguments: \"%s\"\n", arguments);
+            argv[i+1] = NULL;
+                        
             strncpy(command, (const char *)input, (spaceloc -1));
         }
         
-
-
-
         pid_t pid;
 
         if (!strcmp(command, exit)) {
@@ -69,7 +66,7 @@ int main() {
         }
         
         if(!strncmp((const char *)command, cd, maxcomp)){
-            changedir = chdir(argumentsin);
+            changedir = chdir(argv[2]);
             if(changedir){
                 printf("Unable to change to specified directory.");
             }
@@ -83,11 +80,8 @@ int main() {
             fprintf(stderr, "Fork failed\n");
             return -1;
         } else if (pid == 0) {
-            printf("Running...\n");
-            
             if(strcmp(argumentsin, "")){
-                printf("has arguments \"%s\"\n", argumentsin);
-                execlp(command, command, argumentsin, NULL);
+                execvp(command, (char * const*)argv);
             } else {
                 execlp(command, command, NULL);
             }
@@ -95,9 +89,7 @@ int main() {
         } else {
             int result;
             wait(&result);
-            //printf("All done; result = %d\n", result);
         }
-            //printf("running value: %d\n", running);
             
     }
 }
